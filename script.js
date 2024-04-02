6e8e3f0ceeb52106afa56957ad8ada7acd3176eb
Hao Guo <mia0728g@gmail.com>
2024-04-01 23:45:06 -0700

    update index.html

--- script.js
+++ script.js
@@ -4,12 +4,21 @@ var bill = document.querySelector("#bill");
 var tip_percentage = document.querySelector("#tip_percentage");
 var tip_amount = document.querySelector("#tip_amount");
 var total_bill = document.querySelector("#total_bill");
-
+let isNum = /^[0-9]*$/;
 tip_percent.textContent = tp_input.value + "%";
 // tip_percentage.value = tp_input.value + "%";
 bill.addEventListener("input", (event) => {
-    tip_amount.value = (Number(event.target.value) * Number(tp_input.value)/100).toFixed(2);
-    total_bill.value = (Number(event.target.value) + Number(tip_amount.value)).toFixed(2);
+    if(isNum.test(event.target.value)) {
+        document.getElementById("warn").style.visibility="hidden";
+        tip_amount.value = (Number(event.target.value) * Number(tp_input.value)/100).toFixed(2);
+        total_bill.value = (Number(event.target.value) + Number(tip_amount.value)).toFixed(2);
+    }else {
+        // console.log("Only numbers are allowed");
+        document.getElementById("warn").style.visibility="visible";
+        tip_amount.value = "";
+        total_bill.value = "";
+    }
+    
 });
 
 tp_input.addEventListener("input", (event) => {
@@ -18,5 +27,3 @@ tp_input.addEventListener("input", (event) => {
     tip_amount.value = (Number(bill.value) * Number(event.target.value)/100).toFixed(2);
     total_bill.value = (Number(bill.value) + Number(tip_amount.value)).toFixed(2);
 });
-
-console.log(typeof(tip_amount.value));
